dd260f08f2150df5e769933b36a9a095907f3ecc
th <1393015672@qq.com>
2017-09-08 10:49:04 +0800

    111

---
+++ YJLC_E/src/main/java/com/web/vip_auditing_Bean/Member.java
@@ -0,0 +1,156 @@
+package com.web.vip_auditing_Bean;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table
+public class Member {
+
+	private int id; //编号
+	private String member_name;  //姓名
+	private String name; //账号
+	private String password; //密码
+	private String salt; //加密盐
+	private String mobile_Phone; //电话
+	private String status; //状态
+	private String del_flag; //删除标志
+	private String identity; //身份
+	private Date create_date; //创建时间
+	private Date update_date; //修改时间
+	private String weiBoAccount; //微博账号关联
+	private String weixinAccount;//微信账号关联
+	private String headid; //头像
+	private String invitationCode; //邀请码
+	private String withdraw_password; //提款密码
+	private String qqAccount;  //QQ账号关联
+	private String invitedCode; //被邀请码
+	private String qqNumber; //QQ号码
+	
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMember_name() {
+		return member_name;
+	}
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSalt() {
+		return salt;
+	}
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+	public String getMobile_Phone() {
+		return mobile_Phone;
+	}
+	public void setMobile_Phone(String mobile_Phone) {
+		this.mobile_Phone = mobile_Phone;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getDel_flag() {
+		return del_flag;
+	}
+	public void setDel_flag(String del_flag) {
+		this.del_flag = del_flag;
+	}
+	public String getIdentity() {
+		return identity;
+	}
+	public void setIdentity(String identity) {
+		this.identity = identity;
+	}
+	public Date getCreate_date() {
+		return create_date;
+	}
+	public void setCreate_date(Date create_date) {
+		this.create_date = create_date;
+	}
+	public Date getUpdate_date() {
+		return update_date;
+	}
+	public void setUpdate_date(Date update_date) {
+		this.update_date = update_date;
+	}
+	public String getWeiBoAccount() {
+		return weiBoAccount;
+	}
+	public void setWeiBoAccount(String weiBoAccount) {
+		this.weiBoAccount = weiBoAccount;
+	}
+	public String getWeixinAccount() {
+		return weixinAccount;
+	}
+	public void setWeixinAccount(String weixinAccount) {
+		this.weixinAccount = weixinAccount;
+	}
+	public String getHeadid() {
+		return headid;
+	}
+	public void setHeadid(String headid) {
+		this.headid = headid;
+	}
+	public String getInvitationCode() {
+		return invitationCode;
+	}
+	public void setInvitationCode(String invitationCode) {
+		this.invitationCode = invitationCode;
+	}
+	public String getWithdraw_password() {
+		return withdraw_password;
+	}
+	public void setWithdraw_password(String withdraw_password) {
+		this.withdraw_password = withdraw_password;
+	}
+	public String getQqAccount() {
+		return qqAccount;
+	}
+	public void setQqAccount(String qqAccount) {
+		this.qqAccount = qqAccount;
+	}
+	public String getInvitedCode() {
+		return invitedCode;
+	}
+	public void setInvitedCode(String invitedCode) {
+		this.invitedCode = invitedCode;
+	}
+	public String getQqNumber() {
+		return qqNumber;
+	}
+	public void setQqNumber(String qqNumber) {
+		this.qqNumber = qqNumber;
+	}
+	
+	
+	
+	
+}
