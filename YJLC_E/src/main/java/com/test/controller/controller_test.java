95fb11e9d7821766ca9191af334d03e39faf5198
xiongzhaoji <504549658@qq.com>
2017-09-08 16:10:15 +0800

    2017年9月8日16:10:13

---
+++ YJLC_E/src/main/java/com/test/controller/controller_test.java
@@ -0,0 +1,15 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class controller_test {
+
+	@RequestMapping("/sy")
+	public String sy() {
+		System.out.println("aaa");
+		return "homePage";		
+	}
+}
